4975fe6f6a9516d1af68116607d1ce7aec7d28df
Bencio09 <60542213+Bencio09@users.noreply.github.com>
2021-12-23 19:34:08 +0100

    Add files via upload
    jòh

---
+++ SUPERCAR.ino
@@ -0,0 +1,26 @@
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop()
+{
+  for(int i = 2;i<7;i++){
+    digitalWrite(i, HIGH);
+    delay(150);
+    digitalWrite(i, LOW);
+  }
+  
+  for(int i = 7;i>2;i--){
+    
+    digitalWrite(i, HIGH);
+    delay(150);
+    digitalWrite(i, LOW);
+    
+  }
+  
+}
